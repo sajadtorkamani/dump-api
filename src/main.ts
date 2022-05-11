7a7f60185c1286653ca9b8b020af67099ffa912f
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-05-11 11:21:08 +0100

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
+const debug = require('debug')('app:main')
 import app from './app'
 import { connectToDatabase } from './utilities'
 
 const PORT = process.env.PORT || 8000
 
 async function main() {
-  const connection = await connectToDatabase()
-  const [rows] = await connection.query('SELECT * FROM users')
-  console.log({ rows })
+  await connectToDatabase()
+  debug('Connected to database')
 
   app.listen(PORT, () => {
-    console.log(`App listening on http://localhost:${PORT}`)
+    debug(`App listening on http://localhost:${PORT}`)
   })
 }
 
